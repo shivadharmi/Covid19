904fe1e322f1203c62c5b9c92680b036c7bc3756
myshiva59 <53678455+myshiva59@users.noreply.github.com>
2020-03-17 21:49:05 +0530

    Added few images

--- client/src/components/Advices.js
+++ client/src/components/Advices.js
@@ -3,20 +3,25 @@ import React from 'react';
 const Advices = () => {
 	return (
 		<div className='advices'>
-			<div className='card'>
-				<a href='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png'>
-					<img src='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png' />
-				</a>
-			</div>
-			<div className='card'>
-				<a href='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png'>
-					<img src='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png' />
-				</a>
-			</div>
-			<div className='card'>
-				<a href='https://www.who.int/images/default-source/health-topics/coronavirus/wearing-gloves.png'>
-					<img src='https://www.who.int/images/default-source/health-topics/coronavirus/wearing-gloves.png' />
-				</a>
+			<h3>
+				Ask WHO <small>Click on images to view in full screen</small>
+			</h3>
+			<div class='container'>
+				<div className='card'>
+					<a href='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png'>
+						<img src='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png' />
+					</a>
+				</div>
+				<div className='card'>
+					<a href='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png'>
+						<img src='https://www.who.int/images/default-source/health-topics/coronavirus/safe-greetings.png' />
+					</a>
+				</div>
+				<div className='card'>
+					<a href='https://www.who.int/images/default-source/health-topics/coronavirus/wearing-gloves.png'>
+						<img src='https://www.who.int/images/default-source/health-topics/coronavirus/wearing-gloves.png' />
+					</a>
+				</div>
 			</div>
 		</div>
 	);
