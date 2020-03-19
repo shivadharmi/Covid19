fa7a5cb8c9fd2396de8d18fb1b8b83bef5a17b0f
myshiva59 <53678455+myshiva59@users.noreply.github.com>
2020-03-19 22:47:58 +0530

    toggle bug rectified

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,33 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+	var uncollapsed = false;
+	const collapse = () => {
+		if (uncollapsed === false) {
+			document.getElementById('nav-links').style.left = '-100%';
+			uncollapsed = !uncollapsed;
+		} else {
+			document.getElementById('nav-links').style.left = '0';
+			uncollapsed = !uncollapsed;
+		}
+	};
 	return (
 		<nav>
 			<input type='checkbox' id='check'></input>
 			<label htmlFor='check' className='checkbtn'>
-				<i className='fas fa-bars'></i>
+				<i className='fas fa-bars' onClick={collapse}></i>
 			</label>
 			<Link to='/'>
 				<h2 className='nav-brand'>COVID19</h2>
 			</Link>
-			<ul>
+			<ul id='nav-links'>
 				<li>
-					<Link to='/advices' className='nav-link'>
+					<Link to='/advices' className='nav-link' onClick={collapse}>
 						Advices
 					</Link>
 				</li>
 				<li>
-					<Link to='/myths' className='nav-link'>
+					<Link to='/myths' className='nav-link' onClick={collapse}>
 						Myths
 					</Link>
 				</li>
 				<li>
-					<Link to='/statistics' className='nav-link'>
+					<Link to='/statistics' className='nav-link' onClick={collapse}>
 						Statistics
 					</Link>
 				</li>
 				<li>
-					<Link to='/who' className='nav-link'>
+					<Link to='/who' className='nav-link' onClick={collapse}>
 						who
 					</Link>
 				</li>
